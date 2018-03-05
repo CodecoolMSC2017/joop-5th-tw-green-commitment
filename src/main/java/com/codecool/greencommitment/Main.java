4148ff5fb0a8d7b2b5fef11a6327c39b9f99ad1a
Gyorfisti <gyorfisti@gmail.com>
2018-03-05 09:55:44 +0100

    added greencommitment package

---
+++ src/main/java/com/codecool/greencommitment/Main.java
@@ -0,0 +1,10 @@
+package com.codecool.greencommitment;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            //client.Client.start();
+        }
+    }
+}
