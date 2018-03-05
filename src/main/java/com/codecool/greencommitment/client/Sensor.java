0bace99374cfaebf5d1f0740827afefb0be8a2e5
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-03-05 12:16:08 +0100

    Sensor and temperatureSensors's methods updated (added timegetter and randomnumber generator)

--- src/main/java/com/codecool/greencommitment/client/Sensor.java
+++ src/main/java/com/codecool/greencommitment/client/Sensor.java
@@ -2,8 +2,21 @@ package com.codecool.greencommitment.client;
 
 import org.w3c.dom.Document;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Sensor{
+
+    protected String type;
+    protected int id;
 
     // Method(s)
     public abstract Document readData();
+
+    double generateRandomNumber(double minNumber, double maxNumber){
+        return ThreadLocalRandom.current().nextDouble(minNumber, maxNumber);
+    }
+
+    long currentTimeMillis(){
+        return System.currentTimeMillis();
+    }
 }
--- src/main/java/com/codecool/greencommitment/client/TemperatureSensor.java
+++ src/main/java/com/codecool/greencommitment/client/TemperatureSensor.java
@@ -5,8 +5,16 @@ import org.w3c.dom.Document;
 
 public class TemperatureSensor extends Sensor {
 
+    public TemperatureSensor(){
+        this.id = 453;
+        this.type = "celsius";
+    }
+
     @Override
     public Document readData() {
+        double temperature = generateRandomNumber(18, 25);
+        long currentTime = currentTimeMillis();
+
         return null;
     }
 }
