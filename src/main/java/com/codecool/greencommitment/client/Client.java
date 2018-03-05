b2fde3ef4761a4e736dffe3eebeeefdee8ac7756
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-03-05 12:15:08 +0100

    Client id handling partially done

--- src/main/java/com/codecool/greencommitment/client/Client.java
+++ src/main/java/com/codecool/greencommitment/client/Client.java
@@ -1,12 +1,12 @@
 package com.codecool.greencommitment.client;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
+    private String clientId;
     private Socket socket;
     private List<Sensor> sensors;
 
@@ -15,10 +15,51 @@ public class Client {
     public Client(String host, int port) throws IOException{
         socket = new Socket(host, port);
         sensors = new ArrayList<>();
+        handleClientId();
     }
 
 
     // Method(s)
+    private void handleClientId(){
+        String pathToId = "resources/clientid";
+        File idFile = new File(pathToId);
+        if (idFile.exists()){
+            readId(pathToId);
+            sendId();
+        } else {
+            writeId(pathToId, getId());
+        }
+    }
+
+    private void readId(String pathToId){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pathToId));
+            this.clientId = br.readLine();
+            br.close();
+        } catch (IOException e){
+            //Boo!
+        }
+    }
+
+    private void writeId(String pathToId, String id){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(pathToId));
+            bw.write(id);
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            //Boo!
+        }
+    }
+
+    private void sendId(){
+
+    }
+
+    private String getId(){
+
+    }
+
     public void sendData(Sensor sensor) throws IOException {
         try {
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
@@ -28,7 +69,7 @@ public class Client {
 
             out.close();
         } finally {
-            socket.close();
+            //socket.close();
         }
     }
 }
