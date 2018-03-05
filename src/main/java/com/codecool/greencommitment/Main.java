7be32d51f80350f78cc65d17a94c116834220213
Gyorfisti <gyorfisti@gmail.com>
2018-03-05 09:58:19 +0100

    finished Main

--- src/main/java/com/codecool/greencommitment/Main.java
+++ src/main/java/com/codecool/greencommitment/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
             //client.Client.start();
+        } else if (args[0].equals("server")) {
+            //server.Server.start();
         }
     }
 }
