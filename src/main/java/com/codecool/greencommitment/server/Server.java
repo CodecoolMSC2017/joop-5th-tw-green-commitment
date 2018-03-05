bb61e4be56378f823859160d6d91ac2681374be5
Bernáth Máté <bernath.mate.new@gmail.com>
2018-03-05 11:11:05 +0100

    clientSocket filed is added, inputStream, outputStream fields are added, Protocol's constructore is modified, run method is implemetend, readMeasurement() method is implemented

--- src/main/java/com/codecool/greencommitment/server/Server.java
+++ src/main/java/com/codecool/greencommitment/server/Server.java
@@ -3,8 +3,11 @@ package com.codecool.greencommitment.server;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -27,13 +30,11 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ObjectInputStream inputStream;
         while (true) {
             try {
                 Socket clientSocket = serverSocket.accept();
-                inputStream = new ObjectInputStream(clientSocket.getInputStream());
-                new Thread(new Protocol((Document) inputStream.readObject())).start();
-            } catch (IOException | ClassNotFoundException e) {
+                new Thread(new Protocol(clientSocket)).start();
+            } catch (IOException e) {
                 e.printStackTrace();
                 System.exit(0);
             }
@@ -42,13 +43,47 @@ public class Server {
 
     class Protocol implements Runnable {
 
+        private Socket clientSocket;
         private Document document;
+        private ObjectInputStream inputStream;
+        private ObjectOutputStream outputStream;
 
-        Protocol(Document document) {
-            this.document = document;
+        public Protocol(Socket clientSocket) {
+            this.clientSocket = clientSocket;
         }
 
         public void run() {
+            String in = null;
+            try {
+                inputStream = new ObjectInputStream(clientSocket.getInputStream());
+                outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+                in = (String) inputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (in.equals("measurement")) {
+                    readMeasurement();
+                } else if (in.split(" ")[0].equals("request")) {
+                    sendData(in.split(" ")[1]);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void sendData(String id) throws IOException {
+            int idAsInt = Integer.parseInt(id);
+            if (data.containsKey(idAsInt)) {
+                outputStream.writeObject(data.get(idAsInt));
+                return;
+            }
+            outputStream.writeObject(new ArrayList<Integer>());
+        }
+
+        private void readMeasurement() throws IOException, ClassNotFoundException {
+            document = (Document) inputStream.readObject();
             Element measurement = (Element) document.getElementsByTagName("measurement").item(0);
             int id = Integer.parseInt(measurement.getAttribute("id"));
             if (!data.containsKey(id)) {
