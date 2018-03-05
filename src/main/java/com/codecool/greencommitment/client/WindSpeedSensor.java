cb885a57283bafe4a23b74c418e91e096d358bbd
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-03-05 20:42:04 +0100

    Windspeed sensor added - implemeted + gitignore updated with clientid.

---
+++ src/main/java/com/codecool/greencommitment/client/WindSpeedSensor.java
@@ -0,0 +1,28 @@
+package com.codecool.greencommitment.client;
+
+import com.codecool.greencommitment.common.MeasurementParser;
+import org.w3c.dom.Document;
+
+import java.text.DecimalFormat;
+
+public class WindSpeedSensor extends Sensor {
+
+    WindSpeedSensor(){
+        this.id = 1785;
+        this.type = "kph";
+    }
+
+    @Override
+    public Document readData() {
+        Document doc;
+        MeasurementParser parser = new MeasurementParser();
+        DecimalFormat df = new DecimalFormat("#.#");
+
+        double windSpeed = Double.valueOf(df.format(generateRandomNumber(0, 100)));
+        long currentTime = currentTimeMillis();
+
+        doc = parser.createDocument(id, currentTime, windSpeed, type);
+
+        return doc;
+    }
+}
